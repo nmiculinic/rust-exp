6ff0c6e5ff638d73277c0fcadc3e725b737beb9a
Neven Miculinic <neven.miculinic@gmail.com>
2018-12-26 15:07:39 +0100

    Solved ch 13

--- src/sets2.rs
+++ src/sets2.rs
@@ -15,12 +15,13 @@ pub fn pkcs_7_padding(data: &[u8], padding_size: usize) -> Vec<u8> {
     sol
 }
 
+#[derive(Eq, PartialEq, Debug)]
 pub enum Mode {
     CBC,
     ECB,
 }
 
-pub fn encryption_oracle(data: &[u8]) -> (Vec<u8>, Mode) {
+pub fn encryption_oracle(data: &[u8], mode: Mode) -> Vec<u8> {
     let mut rng = rand::thread_rng();
     let mut key: [u8; 16] = [0; 16];
     let mut iv: [u8; 16] = [0; 16];
@@ -29,19 +30,29 @@ pub fn encryption_oracle(data: &[u8]) -> (Vec<u8>, Mode) {
 
     let mut d: Vec<u8> = Vec::new();
     for _ in 0..rng.gen_range(5, 11) {
-        d.push(0);
+        d.push(rng.next_u32() as u8);
     }
     data.iter().for_each(|x| d.push(*x));
     for _ in 0..rng.gen_range(5, 11) {
-        d.push(0);
+        d.push(rng.next_u32() as u8);
     }
-    let g: f64 = rng.gen();
-    if g < 0.5 {
-        let cipher = Cipher::aes_128_cbc();
-        return (encrypt(cipher, &key, Some(&iv), &d).unwrap(), Mode::CBC);
+    let cipher = match mode {
+        Mode::CBC => Cipher::aes_128_cbc(),
+        Mode::ECB => Cipher::aes_128_ecb(),
+    };
+    encrypt(cipher, &key, Some(&iv), &d).unwrap()
+}
+
+pub fn detect_mode<F>(oracle: F) -> Mode
+where
+    F: Fn(&[u8]) -> Vec<u8>,
+{
+    let plaintext: [u8; 1024] = [0; 1024];
+    let cyphertext = oracle(&plaintext);
+    if cyphertext[128..128 + 8] == cyphertext[128 + 16..128 + 16 + 8] {
+        Mode::ECB
     } else {
-        let cipher = Cipher::aes_128_ecb();
-        return (encrypt(cipher, &key, Some(&iv), &d).unwrap(), Mode::ECB);
+        Mode::CBC
     }
 }
 
@@ -69,6 +80,16 @@ mod test {
         let pt = String::from_utf8(decrypt(cipher, key, Some(iv), &data).unwrap()).unwrap();
         // println!("{}", pt);
         // assert!(false) // TODO: Implementet CBC on your own
+    }
+
+    #[test]
+    fn test_ch11() {
+        for _ in 0..10 {
+            assert_eq!(Mode::CBC, detect_mode(|x| encryption_oracle(x, Mode::CBC)),)
+        }
+        for _ in 0..10 {
+            assert_eq!(Mode::ECB, detect_mode(|x| encryption_oracle(x, Mode::ECB)),)
+        }
     }
 
 }
